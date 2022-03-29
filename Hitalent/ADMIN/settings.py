1b4b44bc32ee81ed01f44239afe295e24f06c3a1
iliyas01 <shaikmohammediliyas837@gmail.com>
2022-03-29 14:07:49 +0530

    .

--- Hitalent/ADMIN/settings.py
+++ Hitalent/ADMIN/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-5*pog@^z%q#2os9#2w_y0x11&6-ke4(wrze(z=lsaj#kwu264(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
