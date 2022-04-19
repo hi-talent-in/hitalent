ae73cf59a569f2f138d4b6cf51200cbb4d22f1e5
ILIYAS <mmmiitr@gmail.com>
2022-04-19 14:53:25 +0530

    .

--- Hitalent/UI/models.py
+++ Hitalent/UI/models.py
@@ -20,6 +20,7 @@ class Profile(models.Model):
         ('F', 'Female'),
     )
     LANGUAGE_CHOICES = (
+        ('CHOOSE','Choose')
         ('JAVA', 'Java'),
         ('JAVASCRIPT', 'Javascript'),
         ('PYTHON', 'Python'),
